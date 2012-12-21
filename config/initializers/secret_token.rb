fc819ca52ef59afe67e2dabde41a3019177c2c9a
Billy Irwin <birwin93@gmail.com>
2012-12-21 15:14:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThMoov::Application.config.secret_token = 'ef94eefd119c26216c890dc60d627d05f77ec6ede82088bb615b7353cac05ba3e5e10d3464185d0b8a1a360b283a5f98b9628ac0f2e1a37cfc978f75088cdf45'
